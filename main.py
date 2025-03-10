c19c32edaf7d378b4eb504fb6d177d1588d33d1e
dekarnldy <dekarnldy@gmail.com>
2025-03-10 14:57:31 +0700

    Update gee_utils, landsat_utils, and main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ st.set_page_config(
 
 # Authenticate with Google Earth Engine (Service Account)
 SERVICE_ACCOUNT = "gee-service-account@ee-dekagis.iam.gserviceaccount.com"
-KEY_FILE = os.path.expanduser("~/.config/gee/gee-key.json"
+KEY_FILE = os.path.expanduser("~/.config/gee/gee-key.json")
 
 @st.cache_resource
 def initialize_gee():
