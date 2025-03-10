f25c7a7018888e9752a624a3ac7a8e2eef5c6777
dekarnldy <dekarnldy@gmail.com>
2025-03-10 11:31:24 +0700

    init repo

---
+++ gee_utils.py
@@ -0,0 +1,41 @@
+import ee
+import geemap
+
+def initialize_gee():
+    ee.Authenticate()
+    ee.Initialize(project="ee-dekagis")
+
+
+def create_roi_province(province_name):
+    roi = ee.FeatureCollection("FAO/GAUL/2015/level1") \
+        .filter(ee.Filter.eq('ADM0_NAME', "Indonesia")) \
+        .filter(ee.Filter.eq('ADM1_NAME', province_name))
+    return roi
+
+def get_available_countries():
+    try:
+        countries = ee.FeatureCollection("FAO/GAUL/2015/level0")
+        country_list = countries.aggregate_array('ADM0_NAME').getInfo()
+        return sorted(country_list)
+    except:
+        return ["Indonesia"]
+
+def get_indonesia_provinces():
+    try:
+        provinces = ee.FeatureCollection("FAO/GAUL/2015/level1") \
+            .filter(ee.Filter.eq("ADM0_NAME", "Indonesia"))
+        province_list = provinces.aggregate_array('ADM1_NAME').getInfo()
+        return sorted(province_list)
+    except:
+        return ["Aceh", "Bali", "Banten", "Bengkulu", "DI Yogyakarta", 
+                "DKI Jakarta", "Gorontalo", "Jambi", "Jawa Barat", "Jawa Tengah",
+                "Jawa Timur", "Kalimantan Barat", "Kalimantan Selatan", "Kalimantan Tengah",
+                "Kalimantan Timur", "Kalimantan Utara", "Kepulauan Bangka Belitung",
+                "Kepulauan Riau", "Lampung", "Maluku", "Maluku Utara", "Nusa Tenggara Barat",
+                "Nusa Tenggara Timur", "Papua", "Papua Barat", "Papua Pegunungan",
+                "Papua Tengah", "Papua Selatan", "Papua Barat Daya", "Riau", 
+                "Sulawesi Barat", "Sulawesi Selatan", "Sulawesi Tengah", "Sulawesi Tenggara",
+                "Sulawesi Utara", "Sumatera Barat", "Sumatera Selatan", "Sumatera Utara"]
+
+print(get_available_countries()) 
+print(get_indonesia_provinces()) 
---
+++ landsat_utils.py
@@ -0,0 +1,44 @@
+import ee
+import geemap
+
+def get_landsat_collection(roi, year, max_cloud_cover=5):
+
+    start_date = ee.Date.fromYMD(year, 1, 1)
+    end_date = ee.Date.fromYMD(year, 12, 31)
+
+    landsat_8 = ee.ImageCollection("LANDSAT/LC08/C02/T1_TOA") \
+        .filterBounds(roi) \
+        .filterDate(start_date, end_date) \
+        .filter(ee.Filter.lt('CLOUD_COVER', max_cloud_cover))
+
+    landsat_9 = ee.ImageCollection("LANDSAT/LC09/C02/T1_TOA") \
+        .filterBounds(roi) \
+        .filterDate(start_date, end_date) \
+        .filter(ee.Filter.lt('CLOUD_COVER', max_cloud_cover))
+
+    # combine Landsat 8 and 9
+    if year >= 2021:
+        landsat_combined = landsat_8.merge(landsat_9)
+        return landsat_combined
+    else:
+        return landsat_8
+
+#visualization
+def get_visualization_params(vis_type="true_color"):
+    
+    vis_params = {
+        "true_color": {
+            'bands': ['B4', 'B3', 'B2'],
+            'min': 0,
+            'max': 0.3,
+            'gamma': 1.4
+        },
+        "false_color": {
+            'bands': ['B5', 'B4', 'B3'],
+            'min': 0,
+            'max': 0.3,
+            'gamma': 1.4
+        },
+    }
+    
+    return vis_params.get(vis_type, vis_params["true_color"])
